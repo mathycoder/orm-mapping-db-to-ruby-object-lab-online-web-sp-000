91817c193b81de7d44e7aea75db3742cccac8ad0
Adam S <adam.sarli@gmail.com>
2019-04-10 15:09:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,6 +26,12 @@ class Student
     rows = DB[:conn].execute(sql)
     rows.map{|row| self.new_from_db(row)}
   end 
+  
+  def self.students_below_12th_grade 
+    sql = "SELECT * FROM students WHERE grade < 12"
+    rows = DB[:conn].execute(sql)
+    rows.map{|row| self.new_from_db(row)}
+  end 
 
   def self.find_by_name(name)
     # find the student in the database given a name
