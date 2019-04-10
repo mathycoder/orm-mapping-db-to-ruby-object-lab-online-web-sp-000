528e8e6716df32e9d4f11e53a94d2a14fc89a6f5
Adam S <adam.sarli@gmail.com>
2019-04-10 15:08:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,6 +20,10 @@ class Student
     rows = DB[:conn].execute(sql)
     rows.map{|row| self.new_from_db(row)}
   end
+  
+  def self.all_students_in_grade_9 
+    
+  end 
 
   def self.find_by_name(name)
     # find the student in the database given a name
