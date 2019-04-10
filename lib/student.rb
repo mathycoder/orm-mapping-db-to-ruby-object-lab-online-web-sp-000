89c3068410a87edc5657fd322fca13c91853a1af
Adam S <adam.sarli@gmail.com>
2019-04-10 15:08:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,7 +22,9 @@ class Student
   end
   
   def self.all_students_in_grade_9 
-    
+    sql = "SELECT * FROM students WHERE grade = 9"
+    rows = DB[:conn].execute(sql)
+    rows.map{|row| self.new_from_db(row)}
   end 
 
   def self.find_by_name(name)
