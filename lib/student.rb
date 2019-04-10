47759ed7f6117b92eb5a62f1cf2aeec24ee4964b
Adam S <adam.sarli@gmail.com>
2019-04-10 15:11:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,7 +33,7 @@ class Student
     rows.map{|row| self.new_from_db(row)}
   end 
   
-  def first_student_in_grade_10
+  def self.first_student_in_grade_10
     sql = %{
       SELECT * FROM students 
       WHERE grade = 10 
